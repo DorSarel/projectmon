de544ff502332ad95b4a9d36033df9e4d6c3bf9d
Dor Sarel <Dor.Sarel@wdc.com>
2020-04-26 16:50:30 +0300

    structure Navbar and apply first style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './style.scss';
 
 const Navbar = () => {
-  return <div className='nav-grid'>Navbar</div>;
+  return (
+    <nav className='nav-grid nav'>
+      <Link to='/' className='nav__brand'>
+        Projectmon
+      </Link>
+      <ul className='nav__list'>
+        <li className='nav__item'>
+          {/* <Link to='/' className='nav__link'>
+            Sign in
+          </Link>
+          <Link to='/' className='nav__link'>
+            Sign up
+          </Link> */}
+          <Link to='/' className='nav__link'>
+            Projects
+          </Link>
+          <Link to='/' className='nav__link'>
+            New Project
+          </Link>
+          <Link to='/' className='nav__link'>
+            Log out
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
