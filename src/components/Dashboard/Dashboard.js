6a94a2f81567fd594b4213de9a1aacec1db15f48
Dor Sarel <Dor.Sarel@wdc.com>
2020-04-27 22:43:13 +0300

    Add ProjectsContext to handle all the projects data & actions

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,45 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
+import { projectsContext } from '../../contexts/projectContext';
 import AddNewProject from './AddNewProject';
 import RecentProjects from './RecentProjects';
 import RecentActivities from './RecentActivities';
 
 import './style.scss';
 
-const projects = [
-  {
-    id: 1,
-    title: 'Project 1',
-    description: 'bla bla',
-    activities: [
-      { id: 'a', action: 'add phase' },
-      { id: 'b', action: 'edit phase' },
-      { id: 'c', action: 'add task' },
-    ],
-  },
-  {
-    id: 2,
-    title: 'Project 2',
-    description: 'bla bla',
-    activities: [
-      { id: 'd', action: 'add phase' },
-      { id: 'e', action: 'edit phase' },
-      { id: 'f', action: 'add task' },
-    ],
-  },
-  {
-    id: 3,
-    title: 'Project 3',
-    description: 'bla bla',
-    activities: [
-      { id: 'g', action: 'add phase' },
-      { id: 'h', action: 'edit phase' },
-      { id: 'i', action: 'add task' },
-    ],
-  },
-];
-
 const Dashboard = () => {
+  const { projects } = useContext(projectsContext);
   return (
     <div className='dashboard'>
       <h2 className='recent-title'>Recent Projects</h2>
