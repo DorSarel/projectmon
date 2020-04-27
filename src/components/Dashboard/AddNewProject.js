b4c3daa210c395d8f05b4d909a6670ea36956382
Dor Sarel <Dor.Sarel@wdc.com>
2020-04-27 17:39:03 +0300

    Change class name form project-card to card

--- src/components/Dashboard/AddNewProject.js
+++ src/components/Dashboard/AddNewProject.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AddNewProject = () => {
   return (
-    <div className='project-card new-project dashboard__grid-item'>
+    <div className='card new-project dashboard__grid-item'>
       <i className='fas fa-plus new-project__icon'></i>
       <p className='new-project__text'>add new project</p>
     </div>
--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -3,7 +3,7 @@ import './style.scss';
 
 const Project = ({ project, layoutClass = '' }) => {
   return (
-    <div className={`project-card project ${layoutClass}`}>
+    <div className={`card project ${layoutClass}`}>
       <h3 className='project__title'>{project.title}</h3>
       <span className='project__date'>April 26, 2020</span>
       <p className='project__text'>{project.description}</p>
