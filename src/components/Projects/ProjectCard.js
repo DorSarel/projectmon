648dda8432de004f90098534d78a801cad3e1615
Dor Sarel <Dor.Sarel@wdc.com>
2020-05-02 11:24:47 +0300

    Rename className from project to project-card

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -3,11 +3,11 @@ import './style.scss';
 
 const ProjectCard = ({ project, layoutClass = '' }) => {
   return (
-    <div className={`card project ${layoutClass}`}>
-      <h3 className='project__title'>{project.title}</h3>
-      <span className='project__date'>April 26, 2020</span>
-      <p className='project__text'>{project.description}</p>
-      <button className='btn project__btn'>details</button>
+    <div className={`card project-card ${layoutClass}`}>
+      <h3 className='project-card__title'>{project.title}</h3>
+      <span className='project-card__date'>April 26, 2020</span>
+      <p className='project-card__text'>{project.description}</p>
+      <button className='btn project-card__btn'>details</button>
     </div>
   );
 };
